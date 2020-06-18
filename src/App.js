d219a9870ad2aa590f82625bad47bb1bc883a248
suna-stransa <yuta.sunadoi@stransa.co.jp>
2020-06-18 14:39:14 +0900

    amplify

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  //cookieある時
+  useEffect(() => {
+    fetch('https://tooap4mvb3.execute-api.ap-northeast-1.amazonaws.com/demo', {
+      method: 'POST',
+      body: JSON.stringify({ "sessionId": "123" }),
+      credentials: 'include'
+    })
+    .then(res => res.json())
+    .then(res => {
+        console.log(res);
+    })
+  }, []);
+
+  //cookieない時
+  const apiHandler = () => {
+    fetch('https://tooap4mvb3.execute-api.ap-northeast-1.amazonaws.com/demo', {
+      method: 'POST',
+      body: JSON.stringify({ "sessionId": "123", "authKey": "uuid" }),
+      credentials: 'include',
+    })
+    .then(res => res.json())
+    .then(res => {
+        console.log(res);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit src/App.js and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,11 +46,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 2
         </a>
+        <button onClick={() => apiHandler()}>API送信</button>
       </header>
     </div>
   );
 }
-
 export default App;
