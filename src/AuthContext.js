edd22aee2a87f4cb2bee52a70473f2942994aec0
suna-stransa <yuta.sunadoi@stransa.co.jp>
2020-06-25 14:44:40 +0900

    add comment

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect, createContext} from 'react';
-import Amplify, {Auth} from 'aws-amplify';
+import Amplify, { Auth } from 'aws-amplify';           // amplifyのモジュールの読み込み
+import aws_exports from './aws-exports';               // 設定情報を読み込みます。
+Amplify.configure(aws_exports);                        // 設定情報をAmplifyに反映させます。
 
 export const AuthContext = createContext();
 
@@ -7,28 +9,23 @@ const AuthContextProvider = (props) => {
   const [token, setToken] = useState(null);
 
   const logout = () => {
+    Auth.signOut();
     document.cookie = 'sso-authenticated=false';
+    document.cookie = "officeId=; max-age=0";
     window.location.href = `${process.env.REACT_APP_AUTHENTICATOR_URL}/logout/${encodeURIComponent('http://localhost:3001')}`
   }
-
-  Amplify.configure({
-    Auth: {
-      identityPoolId: 'ap-northeast-1:f247d53d-ffad-4a7d-bae1-941280d750a4',
-      region: 'ap-northeast-1',
-      userPoolId: 'ap-northeast-1_fWEpThYje',
-      userPoolWebClientId: '2nhn4h9lgvi95m03a8nm1lhmht',
-      authenticationFlowType: 'CUSTOM_AUTH',
-    }
-  })
 
   const handleCognitoLogin = async (sessionId, authKey, username) => {
     try {
       const user = await Auth.signIn(username)
+
+      //第二引数がchallengeResponse、LambdaでchallengeAnswerで受け取る。
+      //第三引数がClientMetadata。認証アプリで受け取ったsessionIdを渡すことでこれをキーにしてdynamoにアクセスする。
+      //そのレスポンスのauthKeyと第二引数のauthKeyが一致したら認証成功でtokenがもらえる。
       const customUser = await  Auth.sendCustomChallengeAnswer(user, authKey, {
         sessionId,
       })
       setToken(customUser.signInUserSession.accessToken.jwtToken)
-      localStorage.setItem("test", "true");
       return false;
     } catch(err) {
       console.log(err);
@@ -37,10 +34,23 @@ const AuthContextProvider = (props) => {
   }
 
   useEffect(() => {
+    const isSSOAuthenticated = document.cookie.includes("sso-authenticated=true");
+
     //sso-authenticatedがtrueではない(falseもしくはそもそもない)場合、認証アプリにリダイレクト
-    if (document.cookie !== 'sso-authenticated=true') {
+    if (!isSSOAuthenticated) {
       window.location.href = `${process.env.REACT_APP_AUTHENTICATOR_URL}/login/${encodeURIComponent('http://localhost:3001')}`
     }
+
+    let accessToken = "";
+
+    for( let i=0; i < localStorage.length; i++ ){
+      if (localStorage.key(i).endsWith("accessToken")) {
+        const key = localStorage.key(i)
+        accessToken = localStorage.getItem(key)
+      };
+    }
+
+    // if (!accessToken) ;
 
     // sso-authenticated=trueの場合
     fetch('https://tooap4mvb3.execute-api.ap-northeast-1.amazonaws.com/demo/', {
